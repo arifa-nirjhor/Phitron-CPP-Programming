d5f5ec64c6082558c01368bd38f3a25d65aab940
Nirjhor AR <arifanirjhor@gmail.com>
2025-09-28 19:35:32 +0600

    Add files via upload

---
+++ Introduction to C++ Programming/Modules/Moduels 1 Basic C++/if__else.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x=30;
+    if(x%2==0)
+    {
+        cout<<"Even\n";
+    }
+    else{
+        cout<<"ODD\n";
+    }
+    return 0;
+}
---
+++ Introduction to C++ Programming/Modules/Moduels 1 Basic C++/tarnary.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x=6;
+    x%2==0?cout<<"even":cout<<"odd";
+    return 0;
+}
